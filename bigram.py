274fc911df7661f310f48ed26772acb606039d2b
Alok Vats <53458690+avats77@users.noreply.github.com>
2026-08-16 23:55:28 -0600

    revised version on GPT by Andrej K

--- bigram.py
+++ bigram.py
@@ -6,15 +6,18 @@ from torch.nn import functional as F
 
 
 # hyperparameters
-batch_size = 32
-block_size = 8
-max_iters = 3000
+batch_size = 64
+block_size = 256
+max_iters = 5000
 
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+num_heads = 6
+n_layer = 6
+dropout = 0.2
 #------------------
 
 torch.manual_seed(1337)
@@ -81,6 +84,7 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd, head_size, bias=False)
 
         self.register_buffer('tril', torch.tril(torch.ones(block_size,block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B,T,C = x.shape
@@ -92,11 +96,53 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1) * C**-0.5
         wei = wei.masked_fill(self.tril[:T, :T]== 0 , float('-inf'))
         wei = F.softmax(wei, dim= -1)
+        wei = self.dropout(wei)
         # Perform the weighted aggregation of the values
         v = self.value(x)
         out =  wei @ v
         return out
-    
+
+
+# implementing a multi-head attention model
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim = -1)
+        out = self.dropout(self.proj(out))
+        return out
+
+# A feed forward network
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4* n_embd),
+            nn.ReLU(),
+            nn.Linear(4* n_embd, n_embd),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    def __init__(self, n_embd, num_heads):
+        super().__init__()
+        head_size = n_embd// num_heads
+        self.sa = MultiHeadAttention(num_heads, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))  
+        return x
+
 # super simple bigram model
 
 class BigramLanguageModel(nn.Module):
@@ -104,15 +150,18 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, num_heads = num_heads) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
+
+
 
     def forward(self, idx, targets= None):
         B,T = idx.shape
         tok_emb = self.token_embedding_table(idx)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))
         x = tok_emb + pos_emb # add token and position embeddings
-        x = self.sa_head(x) # apply self attention head
+        x = self.blocks(x) # apply the blocks
         logits = self.lm_head(x)
 
         
